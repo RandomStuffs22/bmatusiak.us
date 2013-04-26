1ca665c30de48dd28361cf5de53228cb5b28f406
Bradley Matusiak <usabrad86@bellsouth.net>
2013-04-26 04:51:01 -0400

    added background emu

--- server-plugins/app.main/client/main.js
+++ server-plugins/app.main/client/main.js
@@ -1,9 +1,21 @@
 define(function(require, exports, module) {
+    require("ext/main/jquery");
+    var termData = require("text!ext/main/termData.txt").split("\n");
+    var currentTermItem = 0;
+    
     return function(options, imports, register) {
         register(null,{
             main:{
                 init:function(callback){
                     console.log("loaded!");
+                    setInterval(function() {
+                        while(($("#underLayer")[0].scrollHeight) > $("#underLayer").height()){
+                            $($("#underLayer div")[0]).remove();
+                        }
+                        console.log($("#underLayer").height(),$("#underLayer")[0].scrollHeight);
+                        $("#underLayer").append("<div>"+termData[currentTermItem]+"</div>");
+                        currentTermItem++;
+                    }, 450);
                 }
             }
         });
