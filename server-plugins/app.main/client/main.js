6c52d92239495eb5651b933ed2099d0003da407a
Bradley Matusiak <usabrad86@bellsouth.net>
2013-04-26 06:50:25 -0400

    its a wrap for now

--- server-plugins/app.main/client/main.js
+++ server-plugins/app.main/client/main.js
@@ -1,13 +1,20 @@
 define(function(require, exports, module) {
     require("ext/main/jquery");
+    require("ext/main/bootstrap");
+    
     var termData = require("text!ext/main/termData.txt").split("\n");
     var currentTermItem = 0;
+    
+    var mainHtml = require("text!ext/main/main.html");
     
     return function(options, imports, register) {
         register(null,{
             main:{
                 init:function(callback){
                     console.log("loaded!");
+                    $("head").prepend("<link rel='stylesheet' href='/static/bootstrap/themes/cyborg/bootstrap.min.css' type='text/css' media='screen' />");
+    
+                    $("#mainpage").html(mainHtml);
                     setInterval(function() {
                         while(($("#underLayer")[0].scrollHeight) > $("#underLayer").height()){
                             $($("#underLayer div")[0]).remove();
